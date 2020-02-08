4ebd000e153ff0457fc10e25c5b293139cc35fb6
k3y5tr0k3 <bryandonnelly7@gmail.com>
2020-02-08 13:53:20 +0000

    app.module.ts updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { TodoAddComponent } from './todo-add/todo-add.component';
+import { TodoService } from './services/todo.service';
 
 @NgModule({
   declarations: [
@@ -14,9 +17,13 @@ import { TodoAddComponent } from './todo-add/todo-add.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    TodoService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
